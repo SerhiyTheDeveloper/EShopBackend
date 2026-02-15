ed8a02d339b97187d53ee87136a19e94a52eee4e
Сергій <vojtujsergij@gmail.com>
2026-02-15 14:46:57 +0200

    added InMemoryOrderRepository

--- MINT.EShop.Infrastracture/Repositories/InMemoryOrderRepository.cs
+++ MINT.EShop.Infrastracture/Repositories/InMemoryOrderRepository.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using MINT.EShop.Core.Entities.Order;
+using MINT.EShop.Core.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MINT.EShop.Infrastracture.Repositories
 {
-    internal class InMemoryOrderRepository
+    public class InMemoryOrderRepository : IOrderRepository
     {
+        private readonly List<Order> _orders = [];
+
+        public Task AddAsync(Order order)
+        {
+            _orders.Add(order);
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteAsync(Guid orderId) 
+        {
+            _orders.RemoveAll(o => o.Id == orderId);
+            return Task.CompletedTask;
+        }
+
+        public Task<IEnumerable<Order>> GetAllAsync() =>
+            Task.FromResult<IEnumerable<Order>>(_orders);
+
+        public Task<Order?> GetByIdAsync(Guid orderId) =>
+            Task.FromResult(_orders.FirstOrDefault(o => o.Id == orderId));
+
+        public Task UpdateAsync(Order order) =>
+            Task.CompletedTask;
     }
 }
