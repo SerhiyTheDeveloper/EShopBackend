e0f01aa2a9f0b3a3d705d4001b7ec6936d284a81
Сергій <vojtujsergij@gmail.com>
2026-02-17 21:55:18 +0200

    added basic DTOs for Order entity

---
+++ MINT.EShop.Business/DTOs/Orders/CreateOrderRequest.cs
@@ -0,0 +1,21 @@
+﻿using MINT.EShop.Business.DTOs.OrderItems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MINT.EShop.Business.DTOs.Orders
+{
+    public record CreateOrderRequest
+    {
+        /// <summary>
+        /// Ідентифікатор клієнта, який створює замовлення (GUID).
+        /// </summary>
+        public required Guid ClientId { get; init; }
+        /// <summary>
+        /// Список товарів у замовленні.
+        /// </summary>
+        public required IReadOnlyList<CreateOrderItemRequest> OrderItems { get; init; }
+    }
+}
---
+++ MINT.EShop.Business/DTOs/Orders/OrderResponse.cs
@@ -0,0 +1,35 @@
+﻿using MINT.EShop.Business.DTOs.OrderItems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MINT.EShop.Business.DTOs.Orders
+{
+    public record OrderResponse
+    {
+        /// <summary>
+        /// Унікальний ідентифікатор (GUID).
+        /// </summary>
+        public required Guid Id { get; init; }
+        /// <summary>
+        /// Ідентифікатор клієнта, який зробив замовлення (GUID).
+        /// </summary>
+        public required Guid ClientId { get; init; }
+        /// <summary>
+        /// Точна дата та час створення замовлення.
+        /// </summary>
+        /// <example>2024-06-01T14:30:00Z</example>
+        public required DateTime OrderDate { get; init; }
+        /// <summary>
+        /// Остаточна вартість замовлення.
+        /// </summary>
+        /// <example>26999.99</example>
+        public required decimal TotalAmount { get; init; }
+        /// <summary>
+        /// Список товарів, включених до замовлення. (Тільки для читання)
+        /// </summary>
+        public required IReadOnlyList<OrderItemResponse> OrderItems { get; init; }
+    }
+}
