c48dafea21d6312a62fc2f0fef350d35fd4aa9a0
Сергій <vojtujsergij@gmail.com>
2026-03-04 18:25:58 +0200

    added new enum OrderStatus

---
+++ MINT.EShop.Core/Enums/OrderStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MINT.EShop.Core.Enums
+{
+    public enum OrderStatus
+    {
+        Pending = 1,
+        Paid = 2,
+        Shipped = 3,
+        Delivered = 4,
+        Cancelled = 0
+    }
+}
