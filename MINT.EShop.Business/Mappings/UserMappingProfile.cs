0178ba42df2968a8eb1c8a3f6021117e3597645f
Сергій <vojtujsergij@gmail.com>
2026-03-19 21:56:17 +0200

    initial implementation for future auto-mapping logic.

---
+++ MINT.EShop.Business/Mappings/UserMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MINT.EShop.Business.Mappings
+{
+    public class UserMappingProfile : Profile
+    {
+        public UserMappingProfile()
+        {
+            CreateMap<DTOs.Identity.RegisterRequest, Core.Entities.UserData.User>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.RegistrationDate, opt => opt.Ignore())
+                .ForMember(dest => dest.Credential, opt => opt.Ignore())
+                .ForMember(dest => dest.Sessions, opt => opt.Ignore());
+        }
+    }
+}
