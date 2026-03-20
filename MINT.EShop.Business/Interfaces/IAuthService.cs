8a2c4fb215611dfaa3674a493f77a36e55174ee9
Сергій <vojtujsergij@gmail.com>
2026-03-20 15:25:24 +0200

    added new interface IAuthController

---
+++ MINT.EShop.Business/Interfaces/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿using MINT.EShop.Business.DTOs.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MINT.EShop.Business.Interfaces
+{
+    public interface IAuthService
+    {
+        public Task<LoginResponse?> Login(LoginRequest request);
+    }
+}
