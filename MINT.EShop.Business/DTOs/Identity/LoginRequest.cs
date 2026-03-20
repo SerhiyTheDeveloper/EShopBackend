4686a43e048fdca5ce806bb578a351463b03eda8
Сергій <vojtujsergij@gmail.com>
2026-03-20 15:26:46 +0200

    added new DTOs LoginRequest and LoginResponse

---
+++ MINT.EShop.Business/DTOs/Identity/LoginRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MINT.EShop.Business.DTOs.Identity
+{
+    /// <summary>
+    /// Дані для входу в систему
+    /// </summary>
+    public record LoginRequest
+    {
+        /// <summary>
+        /// Електронна пошта.
+        /// </summary>
+        /// <example>user@example.com</example>
+        public required string Email { get; init; }
+        /// <summary>
+        /// Пароль.
+        /// </summary>
+        /// <example>StrPassword123</example>
+        [MinLength(8)]
+        public required string Password { get; init; }
+    }
+}
---
+++ MINT.EShop.Business/DTOs/Identity/LoginResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MINT.EShop.Business.DTOs.Identity
+{
+    /// <summary>
+    /// Відповідь після успішної автентифікації користувача
+    /// </summary>
+    public record LoginResponse
+    {
+        /// <summary>
+        /// Токен для автентифікації запитів
+        /// </summary>
+        public required string AccessToken { get; init; }
+        /// <summary>
+        /// Токен для оновлення сесії після закінчення дії AccessToken
+        /// </summary>
+        public required string RefreshToken { get; init; }
+        /// <summary>
+        /// Дата та час закінчення дії RefreshToken (UTC)
+        /// </summary>
+        public required DateTime ExpiresDate { get; init; }
+    }
+}
