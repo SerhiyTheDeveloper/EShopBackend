97ae74d9b501bdd4d1f74ca6c78d29a084a1b34a
Сергій <vojtujsergij@gmail.com>
2026-03-21 21:07:31 +0200

    added new class JwtProvider for generating JWT.

---
+++ MINT.EShop.Infrastracture/JwtProvider.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using MINT.EShop.Core.Entities.UserData;
+using MINT.EShop.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MINT.EShop.Infrastracture
+{
+    public class JwtProvider(IOptions<JwtOptions> options) : IJwtProvider
+    {
+        private readonly JwtOptions _options = options.Value;
+
+        public string GenerateToken(User user)
+        {
+            Claim[] claims = [new("id", user.Id.ToString())];
+
+            var signingCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.SecretKey)),
+                SecurityAlgorithms.HmacSha256);
+
+            var jwtSecurityToken = new JwtSecurityToken(
+                claims: claims,
+                signingCredentials: signingCredentials,
+                expires: DateTime.UtcNow.AddMinutes(_options.ExpiresInMinutes));
+
+            var token = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+
+            return token;
+        }
+    }
+}
