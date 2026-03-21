cc92eb33b3176dafec5625a9bfb6ca805dec7205
Сергій <vojtujsergij@gmail.com>
2026-03-21 21:09:48 +0200

    created JwtOptions that takes values from appsettings for JwtProvider.

---
+++ MINT.EShop.Infrastracture/JwtOptions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MINT.EShop.Infrastracture
+{
+    public class JwtOptions
+    {
+        public string SecretKey { get; set; } = string.Empty;
+        public int ExpiresInMinutes { get; set; }
+    }
+}
