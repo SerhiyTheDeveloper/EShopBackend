a585665d929c238fcf39212a1a9021899f28f42f
Сергій <vojtujsergij@gmail.com>
2026-03-21 19:18:05 +0200

    implemented PasswordHasher

---
+++ MINT.EShop.Infrastracture/PasswordHasher.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using MINT.EShop.Business.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MINT.EShop.Infrastracture
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string Hash(string password)
+        {
+            string passwordHash = BCrypt.Net.BCrypt.EnhancedHashPassword(password);
+            return passwordHash;
+        }
+
+        public bool Verify(string password, string passwordHash)
+        {
+            bool isMatched = BCrypt.Net.BCrypt.EnhancedVerify(password, passwordHash);
+            return isMatched;
+        }
+    }
+}
