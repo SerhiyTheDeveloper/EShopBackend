96d000a0af8be67df47026fbfcbae049400363cf
Сергій <vojtujsergij@gmail.com>
2026-03-21 21:10:25 +0200

    added interface for JwtProvider.

---
+++ MINT.EShop.Core/Interfaces/IJwtProvider.cs
@@ -0,0 +1,14 @@
+﻿using MINT.EShop.Core.Entities.UserData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MINT.EShop.Core.Interfaces
+{
+    public interface IJwtProvider
+    {
+        public string GenerateToken(User user);
+    }
+}
