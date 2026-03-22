021d32fb2d14482dd88197339b20d279dace218f
Сергій <vojtujsergij@gmail.com>
2026-03-22 11:56:18 +0200

    Add configuratios to Order(needs update later), OrderItem, Product

---
+++ MINT.EShop.Infrastracture/Configurations/OrderConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MINT.EShop.Core.Entities.Order;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MINT.EShop.Infrastracture.Configurations
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.HasKey(o => o.Id);
+
+            builder.Property(o => o.TotalAmount)
+                .IsRequired()
+                .HasColumnType("decimal(18,2)");
+
+            builder.Property(o => o.OrderDate)
+                .IsRequired();
+
+            // Зв'язкок до акаунта клієнта
+        }
+    }
+}
