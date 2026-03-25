73dd5833b8867c896bcfa01c9405474bebe02bdb
Сергій <vojtujsergij@gmail.com>
2026-03-25 22:28:48 +0200

    Add filter for correct swagger authorization display

---
+++ MINT.EShop.API/Filters/SecurityRequirementsOperationFilter.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace MINT.EShop.API.Filters
+{
+    public class SecurityRequirementsOperationFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            // Перевіряємо, чи має метод або його контролер атрибут [Authorize]
+            var hasAuthorize = context.MethodInfo.DeclaringType!.GetCustomAttributes(true).OfType<AuthorizeAttribute>().Any() ||
+                               context.MethodInfo.GetCustomAttributes(true).OfType<AuthorizeAttribute>().Any();
+
+            // Перевіряємо, чи немає там [AllowAnonymous] (він перебиває Authorize)
+            var hasAllowAnonymous = context.MethodInfo.GetCustomAttributes(true).OfType<AllowAnonymousAttribute>().Any();
+
+            if (hasAuthorize && !hasAllowAnonymous)
+            {
+                operation.Security ??= new List<OpenApiSecurityRequirement>();
+
+                var scheme = new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
+                };
+
+                operation.Security.Add(new OpenApiSecurityRequirement
+                {
+                    [scheme] = Array.Empty<string>()
+                });
+            }
+        }
+    }
+}
