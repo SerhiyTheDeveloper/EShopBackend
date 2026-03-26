99c89264466f0535d8cd59adc6cea7ba8115057d
Сергій <vojtujsergij@gmail.com>
2026-03-26 20:08:41 +0200

    Add new enum "Role"

---
+++ MINT.EShop.Core/Enums/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MINT.EShop.Core.Enums
+{
+    public enum Role
+    {
+        Admin = 0,
+        Manager = 1,
+        Client = 2
+    }
+}
