07f7bde23ccfaf8b4370448b0e4fdad053b0774f
Сергій <vojtujsergij@gmail.com>
2026-04-02 15:48:14 +0300

    Add new dto for filtration orders

---
+++ MINT.EShop.Business/DTOs/Orders/GetOrderFilter.cs
@@ -0,0 +1,17 @@
+﻿using MINT.EShop.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MINT.EShop.Business.DTOs.Orders
+{
+    public record GetOrdersFilter
+    {
+        /// <summary>
+        /// Статус замовлення (Cancelled, Pending, Paid, Shipped, Delivered).
+        /// </summary>
+        public OrderStatus? Status { get; init; }
+    }
+}
