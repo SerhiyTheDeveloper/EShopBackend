a175269e7d4b535ba162560cfe09ba10c56fe951
Сергій <vojtujsergij@gmail.com>
2026-04-02 15:32:55 +0300

    Add ManagerId field to know who added this product

--- MINT.EShop.Core/Entities/Product.cs
+++ MINT.EShop.Core/Entities/Product.cs
@@ -13,5 +13,6 @@ namespace MINT.EShop.Core.Entities
         public required string Description { get; set; }
         public required decimal Price { get; set; }
         public required int Stock { get; set; }
+        public Guid? ManagerId { get; set; }
     }
 }
