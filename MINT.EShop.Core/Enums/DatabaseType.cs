33465c37e6108accea3ba85d7caa7ddc4e7c2697
Сергій <vojtujsergij@gmail.com>
2026-04-02 15:36:28 +0300

    Add new enum which represents the type of database

---
+++ MINT.EShop.Core/Enums/DatabaseType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MINT.EShop.Core.Enums
+{
+    public enum DatabaseType
+    {
+        InMemory = 0,
+        PostgreSQL = 1,
+    }
+}
