94581bbb140973a5b89e664b1f5d6ef56a0bc7a9
Сергій <vojtujsergij@gmail.com>
2026-04-02 15:37:49 +0300

    Add admin options for further use

---
+++ MINT.EShop.Infrastracture/AdminOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MINT.EShop.Infrastracture
+{
+    public class AdminOptions
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+    }
+}
