c439e13417a8adb5eb1f486fbb5327e8d3fc41f9
Сергій <vojtujsergij@gmail.com>
2026-04-02 15:49:54 +0300

    Configure new methods from ApiExtentions

--- MINT.EShop.API/Program.cs
+++ MINT.EShop.API/Program.cs
@@ -1,25 +1,23 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.WebUtilities;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
+using MINT.EShop.API.Extensions;
 using MINT.EShop.API.Middlewares;
-using MINT.EShop.API.Wrappers;
 using MINT.EShop.Business.Interfaces;
 using MINT.EShop.Business.Services;
+using MINT.EShop.Core.Enums;
 using MINT.EShop.Core.Interfaces;
 using MINT.EShop.Infrastracture;
-using System.Reflection;
-using System.Text;
-using System.Text.Json.Serialization;
-using MINT.EShop.API.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("JwtSettings"));
+builder.Services.Configure<AdminOptions>(builder.Configuration.GetSection("AdminSetup"));
 
-builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("InMemoryDb"));
+var dbType = (DatabaseType)builder.Configuration.GetValue<int>("DatabaseType");
+
+builder.Host.UseSerilogExtension(builder.Configuration, dbType);
+
+builder.Services.AddDbContextExtension(builder.Configuration, dbType);
+
+builder.Services.AddScoped<IDataSeeder, DataSeeder>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
@@ -28,78 +26,19 @@ builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
 builder.Services.AddScoped<IJwtProvider, JwtProvider>();
 
-builder.Services.AddControllers()
-    .AddJsonOptions(options => {
-        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-    })
-    .ConfigureApiBehaviorOptions(options => {
-        options.InvalidModelStateResponseFactory = context =>
-        {
-            var errors = context.ModelState.Values
-                .SelectMany(v => v.Errors)
-                .Select(e => e.ErrorMessage)
-                .ToList();
-
-            var response = APIResponse.FailureResponse("Validation failed", errors);
-
-            return new BadRequestObjectResult(response);
-        };
-        options.SuppressMapClientErrors = true;
-    });
+builder.Services.AddControllersExtension();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c => {
-    c.SupportNonNullableReferenceTypes();
+builder.Services.AddSwaggerGenExtension();
 
-    var apiXml = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, apiXml));
+builder.Services.AddAuthenticationExtension(builder.Configuration);
 
-    var businessXml = "MINT.EShop.Business.xml";
-    c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, businessXml));
-
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.Http,
-        Scheme = "Bearer",
-        BearerFormat = "Jwt",
-        In = ParameterLocation.Header,
-        Description = "Enter pure JwtToken below."
-    });
-
-    c.OperationFilter<SecurityRequirementsOperationFilter>();
-});
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
-    {
-        options.TokenValidationParameters = new()
-        {
-            ValidateIssuer = false,
-            ValidateAudience = false,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:SecretKey"]!))
-        };
-    });
-
-builder.Services.AddAuthorizationBuilder()
-    .AddPolicy("AdminPolicy", policy =>
-    {
-        policy.RequireClaim("Admin", "true");
-    })
-    .AddPolicy("ManagerPolicy", policy =>
-    {
-        policy.RequireClaim("Manager", "true");
-    })
-    .AddPolicy("ClientPolicy", policy =>
-    {
-        policy.RequireClaim("Client", "true");
-    });
+builder.Services.AddAuthorizationBuilderExtension();
 
 var app = builder.Build();
 
 app.UseMiddleware<GlobalExceptionHandler>();
+
+await app.UseDbInitializerExtension();
 
 if (app.Environment.IsDevelopment())
 {
@@ -109,18 +48,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseStatusCodePages(async context =>
-{
-    context.HttpContext.Response.ContentType = "application/json";
-
-    var statusCode = context.HttpContext.Response.StatusCode;
-
-    string message = ReasonPhrases.GetReasonPhrase(statusCode);
-
-    var response = APIResponse.FailureResponse(message);
-
-    await context.HttpContext.Response.WriteAsJsonAsync(response);
-});
+app.UseStatusCodePagesExtension();
 
 app.UseAuthentication();
 app.UseAuthorization();
