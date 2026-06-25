f27e75079b1d8466951f7d72c6d14d3bc593c38a
Сергій <vojtujsergij@gmail.com>
2026-06-25 18:04:48 +0300

    Create Category and Producer repositories

---
+++ MINT.EShop.Infrastracture/Repositories/CategoryRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MINT.EShop.Core.Entities.Product;
+using MINT.EShop.Core.Interfaces;
+
+namespace MINT.EShop.Infrastracture.Repositories
+{
+    public class CategoryRepository(AppDbContext dbContext) : ICategoryRepository
+    {
+        public async Task AddAsync(Category category)
+        {
+            await dbContext.Categories.AddAsync(category);
+        }
+
+        public void Delete(Category category)
+        {
+            dbContext.Categories.Remove(category);
+        }
+
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            return await dbContext.Categories.ToListAsync();
+        }
+
+        public Task<Category?> GetByIdAsync(Guid categoryId)
+        {
+            return dbContext.Categories.FirstOrDefaultAsync(c => c.Id == categoryId);
+        }
+
+        public void Update(Category category)
+        {
+            dbContext.Categories.Update(category);
+        }
+    }
+}
---
+++ MINT.EShop.Infrastracture/Repositories/ProducerRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MINT.EShop.Core.Entities.Product;
+using MINT.EShop.Core.Interfaces;
+
+namespace MINT.EShop.Infrastracture.Repositories
+{
+    public class ProducerRepository(AppDbContext dbContext) : IProducerRepository
+    {
+        public async Task AddAsync(Producer producer)
+        {
+            await dbContext.Producers.AddAsync(producer);
+        }
+
+        public void Delete(Producer producer)
+        {
+            dbContext.Producers.Remove(producer);
+        }
+
+        public async Task<IEnumerable<Producer>> GetAllAsync()
+        {
+            return await dbContext.Producers.ToListAsync();
+        }
+
+        public Task<Producer?> GetByIdAsync(Guid producerId)
+        {
+            return dbContext.Producers.FirstOrDefaultAsync(p => p.Id == producerId);
+        }
+
+        public void Update(Producer producer)
+        {
+            dbContext.Producers.Update(producer);
+        }
+    }
+}
