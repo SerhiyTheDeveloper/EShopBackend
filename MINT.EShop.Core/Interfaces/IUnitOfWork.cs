9c92f7e8867c84a9d7c01f5bcfc29bbd9f35426e
Сергій <vojtujsergij@gmail.com>
2026-06-25 18:06:24 +0300

    Updated UnitOfWork and his interface

--- MINT.EShop.Core/Interfaces/IUnitOfWork.cs
+++ MINT.EShop.Core/Interfaces/IUnitOfWork.cs
@@ -5,6 +5,8 @@
         public IUserRepository Users { get; }
         public IProductRepository Products { get; }
         public IOrderRepository Orders { get; }
+        public ICategoryRepository Categories { get; }
+        public IProducerRepository Producers { get; }
         Task<int> CompleteAsync();
     }
 }
--- MINT.EShop.Infrastracture/UnitOfWork.cs
+++ MINT.EShop.Infrastracture/UnitOfWork.cs
@@ -10,12 +10,14 @@ namespace MINT.EShop.Infrastracture
         private IUserRepository? _userRepository;
         private IProductRepository? _productRepository;
         private IOrderRepository? _orderRepository;
+        private ICategoryRepository? _categoryRepository;
+        private IProducerRepository? _producerRepository;
 
         public IUserRepository Users => _userRepository ??= new UserRepository(_dbContext);
-
         public IProductRepository Products => _productRepository ??= new ProductRepository(_dbContext);
-
         public IOrderRepository Orders => _orderRepository ??= new OrderRepository(_dbContext);
+        public ICategoryRepository Categories => _categoryRepository ??= new CategoryRepository(_dbContext);
+        public IProducerRepository Producers => _producerRepository ??= new ProducerRepository(_dbContext);
 
         public async Task<int> CompleteAsync()
         {
