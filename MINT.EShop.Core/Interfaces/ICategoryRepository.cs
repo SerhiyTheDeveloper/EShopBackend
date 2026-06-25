e9e78378140c80f36294f93610a2734497839cf1
Сергій <vojtujsergij@gmail.com>
2026-06-25 16:59:11 +0300

    Create interfaces for repositories for Categories and Producers

---
+++ MINT.EShop.Core/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using MINT.EShop.Core.Entities.Product;
+
+namespace MINT.EShop.Core.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<Category?> GetByIdAsync(Guid categoryId);
+        Task<IEnumerable<Category>> GetAllAsync();
+        Task AddAsync(Category category);
+        void Update(Category category);
+        void Delete(Category category);
+    }
+}
---
+++ MINT.EShop.Core/Interfaces/IProducerRepository.cs
@@ -0,0 +1,13 @@
+﻿using MINT.EShop.Core.Entities.Product;
+
+namespace MINT.EShop.Core.Interfaces
+{
+    public interface IProducerRepository
+    {
+        Task<Producer?> GetByIdAsync(Guid producerId);
+        Task<IEnumerable<Producer>> GetAllAsync();
+        Task AddAsync(Producer producer);
+        void Update(Producer producer);
+        void Delete(Producer producer);
+    }
+}
